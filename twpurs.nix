01ea48d7b35be9f364dcc2ee5ea3bc9e0883501c
gillchristian <gillchristiang@gmail.com>
2020-12-29 23:54:20 +0100

    release(nix): Update twpurs.nix

--- twpurs.nix
+++ twpurs.nix
@@ -1,30 +1,30 @@
-{ mkDerivation, base, casing, containers, directory, fetchgit
-, filepath, hpack, hspec, MissingH, optparse-applicative, parsec
-, stdenv, text
+{ mkDerivation, base, casing, containers, data-default, directory
+, fetchgit, filepath, hpack, hspec, MissingH, optparse-applicative
+, parsec, stdenv, stm, text, twitch
 }:
 mkDerivation {
   pname = "tailwind-purs";
   version = "0.0.1.0";
   src = fetchgit {
     url = "https://github.com/gillchristian/tailwind-purs.git";
-    sha256 = "0z48wsy4xqrd7pgw5wnqy36xwdzrmi6xbp4cwh37iagjkavqkgqs";
-    rev = "5d6ab2bf609c83d9baee64740ecd62270bf4c5f1";
+    sha256 = "027x2b8kxc3q3xf4i4jhk94x5mh5ri02386xh62sqf74zgmpdgbq";
+    rev = "3b5bcfebf891c7c0eae1ae4991af29e411b63fdd";
     fetchSubmodules = true;
   };
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    base casing containers directory filepath MissingH
-    optparse-applicative parsec text
+    base casing containers data-default directory filepath MissingH
+    optparse-applicative parsec stm text twitch
   ];
   libraryToolDepends = [ hpack ];
   executableHaskellDepends = [
-    base casing containers directory filepath MissingH
-    optparse-applicative parsec text
+    base casing containers data-default directory filepath MissingH
+    optparse-applicative parsec stm text twitch
   ];
   testHaskellDepends = [
-    base casing containers directory filepath hspec MissingH
-    optparse-applicative parsec text
+    base casing containers data-default directory filepath hspec
+    MissingH optparse-applicative parsec stm text twitch
   ];
   prePatch = "hpack";
   homepage = "https://github.com/gillchristian/tailwind-purs#readme";
