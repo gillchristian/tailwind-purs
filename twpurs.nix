be5a4b9e8a736dda0c0e99009e811fa116de567c
gillchristian <gillchristiang@gmail.com>
2021-02-25 23:05:39 +0100

    release(nix): Update twpurs.nix

--- twpurs.nix
+++ twpurs.nix
@@ -8,8 +8,8 @@ mkDerivation {
   version = "0.0.1.0";
   src = fetchgit {
     url = "https://github.com/gillchristian/tailwind-purs.git";
-    sha256 = "07121alk2pd7dw0r43bbqldchm2rxf71k53l03p35ms0a8p0w40b";
-    rev = "0a5f79874a8e6571b1e861a51a46b49a27f0ebe5";
+    sha256 = "187lnpxirhsf266b1cmbm6pkdyln4nyssyzdn8qgwgnyg1b5jpir";
+    rev = "c83e939f86bd0dd4f5a7ab9afaf991621aeee852";
     fetchSubmodules = true;
   };
   isLibrary = true;
