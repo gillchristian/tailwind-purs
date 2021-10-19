ddd12b50623f47cd24e3d9525a9f7433100ef2a6
Barry Moore <3086255+chiroptical@users.noreply.github.com>
2021-10-19 09:24:52 -0400

    Use with-utf8 (#9)

--- twpurs.nix
+++ twpurs.nix
@@ -1,36 +1,31 @@
 { mkDerivation, base, casing, containers, data-default, directory
-, fetchgit, filepath, hpack, hspec, html-parse, MissingH, mtl
-, optparse-applicative, parsec, stdenv, stm, text, transformers
-, twitch
+, filepath, hpack, hspec, html-parse, lib, MissingH, mtl
+, optparse-applicative, parsec, stm, text, transformers, twitch
+, with-utf8
 }:
 mkDerivation {
   pname = "tailwind-purs";
   version = "0.0.1.0";
-  src = fetchgit {
-    url = "https://github.com/gillchristian/tailwind-purs.git";
-    sha256 = "0ghraxnfb93jlgwjw66vpkmxlxqjsz7h08wqdm1fl3h6ikxr5cz0";
-    rev = "5db77c8755f1d8bdc8efd1217df673e8b9cc99b0";
-    fetchSubmodules = true;
-  };
+  src = ./.;
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
     base casing containers data-default directory filepath html-parse
     MissingH mtl optparse-applicative parsec stm text transformers
-    twitch
+    twitch with-utf8
   ];
   libraryToolDepends = [ hpack ];
   executableHaskellDepends = [
     base casing containers data-default directory filepath html-parse
     MissingH mtl optparse-applicative parsec stm text transformers
-    twitch
+    twitch with-utf8
   ];
   testHaskellDepends = [
     base casing containers data-default directory filepath hspec
     html-parse MissingH mtl optparse-applicative parsec stm text
-    transformers twitch
+    transformers twitch with-utf8
   ];
   prePatch = "hpack";
   homepage = "https://github.com/gillchristian/tailwind-purs#readme";
-  license = stdenv.lib.licenses.mit;
+  license = lib.licenses.mit;
 }
